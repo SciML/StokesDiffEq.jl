5fd2e6224fb7f864923152985e26c12d77d6c780
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-05-04 05:30:54 -0700

    update to DiffEqBase 1.0.0

--- src/problems.jl
+++ src/problems.jl
@@ -1,47 +1,49 @@
-type FDMMesh
-  Δxs::AbstractArray
-  mins::AbstractArray
-  maxs::AbstractArray
+type FDMMesh{X,MIN,MAX}
+  Δxs::X
+  mins::MIN
+  maxs::MAX
   grids::Tuple
   dims::Int
   gridSize::Tuple
   square::Bool
-  function FDMMesh(Δxs::AbstractArray;mins=[0;0],maxs=[1;1],buildMesh=true)
-    if length(mins)!=length(maxs) || length(Δxs)!=length(mins) error("DimensionMismatch") end
-    dims = length(mins)
-    if buildMesh && dims == 2
-      grids = meshgrid(mins[1]:Δxs[1]:maxs[1],mins[2]:Δxs[2]:maxs[2])
-      gridSize = size(grids[1])
-    elseif buildMesh && dims == 3
-      grids = meshgrid(mins[1]:Δxs[1]:maxs[1],mins[2]:Δxs[2]:maxs[2],mins[3]:Δxs[3]:maxs[3])
-      gridSize = size(grids[1])
-    else
-      grids = nothing
-      gridSize = (maxs-mins)./Δxs
-    end
-    square = minimum(map((x)->(x == gridSize[1]),gridSize))
-    new(Δxs,mins,maxs,grids,dims,gridSize,square)
-  end
-  FDMMesh(Δx::Number;mins=[0;0],maxs=[1;1],buildMesh=true) = FDMMesh(Δx*ones(mins),mins=mins,maxs=maxs,buildMesh=buildMesh)
 end
+
+function FDMMesh(Δxs::AbstractArray;mins=[0;0],maxs=[1;1],buildMesh=true)
+  if length(mins)!=length(maxs) || length(Δxs)!=length(mins) error("DimensionMismatch") end
+  dims = length(mins)
+  if buildMesh && dims == 2
+    grids = meshgrid(mins[1]:Δxs[1]:maxs[1],mins[2]:Δxs[2]:maxs[2])
+    gridSize = size(grids[1])
+  elseif buildMesh && dims == 3
+    grids = meshgrid(mins[1]:Δxs[1]:maxs[1],mins[2]:Δxs[2]:maxs[2],mins[3]:Δxs[3]:maxs[3])
+    gridSize = size(grids[1])
+  else
+    grids = nothing
+    gridSize = (maxs-mins)./Δxs
+  end
+  square = minimum(map((x)->(x == gridSize[1]),gridSize))
+  FDMMesh(Δxs,mins,maxs,grids,dims,gridSize,square)
+end
+FDMMesh(Δx::Number;mins=[0;0],maxs=[1;1],buildMesh=true) = FDMMesh(Δx*ones(mins),mins=mins,maxs=maxs,buildMesh=buildMesh)
 
 """
 `size(mesh::FDMMesh)`
 
 Returns `gridSize`.
 """
-size(mesh::FDMMesh) = mesh.gridSize
+Base.size(mesh::FDMMesh) = mesh.gridSize
 
-type StokesProblem
-  f₁#::Function
-  f₂#::Function
-  g#::Function
-  ugD#::Function
-  vgD#::Function
-  uanalytic#::Function
-  vanalytic#::Function
-  panalytic#::Function
+type StokesProblem{F1,F2,G,Ug,Vg,UA,VA,PA}
+  f₁::F1
+  f₂::F2
+  g::G
+  ugD::Ug
+  vgD::Vg
+  uanalytic::UA
+  vanalytic::VA
+  panalytic::PA
   trueknown::Bool
-  StokesProblem(f₁,f₂,g,uanalytic,vanalytic,panalytic) = new(f₁,f₂,g,uanalytic,vanalytic,uanalytic,vanalytic,panalytic,true)
-  StokesProblem(f₁,f₂,g,ugD,vgD) = new(f₁,f₂,g,ugD,vgD,nothing,nothing,nothing,false)
 end
+
+StokesProblem(f₁,f₂,g,uanalytic,vanalytic,panalytic) = StokesProblem(f₁,f₂,g,uanalytic,vanalytic,uanalytic,vanalytic,panalytic,true)
+StokesProblem(f₁,f₂,g,ugD,vgD) = StokesProblem(f₁,f₂,g,ugD,vgD,nothing,nothing,nothing,false)
--- src/solutions.jl
+++ src/solutions.jl
@@ -17,16 +17,18 @@ Holds the data for the solution to a Stokes problem.
 * converrors
 
 """
-type StokesSolution <: DESolution
-  u
-  v
-  p
-  u_analytic
-  vTrue
-  pTrue
+type StokesSolution{T,N,U,V,P,E,C} <: AbstractNoTimeSolution{T,N}
+  u::Array{T,N}
+  v::Array{T,N}
+  p::Array{T,N}
+  u_analytic::U
+  vTrue::V
+  pTrue::P
   mesh::FDMMesh
   trueknown::Bool
-  errors
-  converrors
-  StokesSolution(u,v,p,u_analytic,vTrue,pTrue,mesh,trueknown;errors=nothing,converrors=nothing) = new(u,v,p,u_analytic,vTrue,pTrue,mesh,trueknown,errors,converrors)
+  errors::E
+  converrors::C
 end
+
+  StokesSolution(u,v,p,u_analytic,vTrue,pTrue,mesh,trueknown;errors=nothing,converrors=nothing) =
+  StokesSolution(u,v,p,u_analytic,vTrue,pTrue,mesh,trueknown,errors,converrors)
